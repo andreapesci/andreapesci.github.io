97390b17c756e97915f5c9fb0559dddf07ea1e80
Andrea Pesci <andrea@Andreas-Mac-Studio.local>
2024-05-24 16:47:44 +0200

    all starting elements are added

--- app/main.py
+++ app/main.py
@@ -6,11 +6,22 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/resume", response_class=HTMLResponse)
+async def read_about(request: Request):
+    return templates.TemplateResponse("resume.html", {"request": request})
+
+@app.get("/home", response_class=HTMLResponse)
+async def read_contact(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
 
 @app.get("/hello", response_class=HTMLResponse)
 async def say_hello(request: Request):
