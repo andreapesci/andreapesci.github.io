35dc99c37281cd190e5a1e157c7b5182c31f2813
bitinformatica-dev <138016144+bitinformatica-dev@users.noreply.github.com>
2024-06-05 13:57:58 +0200

    Start working on new layout

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
+import jinja_partials
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from jinja2_fragments.fastapi import Jinja2Blocks
-import jinja_partials
 
-from app.services.portfolio_service import all_projects
 from app.services.blog_service import all_blogs
+from app.services.portfolio_service import all_projects
 
 app = FastAPI()
 
@@ -15,11 +15,14 @@ templates = Jinja2Blocks(directory="app/templates")
 
 jinja_partials.register_starlette_extensions(templates)
 
+
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     portfolio = all_projects()
     blog = all_blogs()
+    stringatest = "questa  è una stringa molto molto lunga per testare se il plugin di black funziona oppure no, cosi da vedere se fa format"
     return templates.TemplateResponse("home/index.html", {"request": request, "portfolio": portfolio})
+
 
 # @app.get("/resume", response_class=HTMLResponse)
 # async def read_about(request: Request):
@@ -30,4 +33,3 @@ async def read_root(request: Request):
 #     portfolio = all_projects()
 #     blog = all_blogs()
 #     return templates.TemplateResponse("home/index.html", {"request": request, "portfolio": portfolio, "blog":blog})
-
---
+++ testhtml/script.js
@@ -0,0 +1,9 @@
+// jsfunctions.js
+function myFunction() {
+    alert("Funzione JavaScript chiamata!");
+}
+
+function updateContent() {
+    document.getElementById('some-div').innerHTML = 'Contenuto aggiornato!';
+}
+
