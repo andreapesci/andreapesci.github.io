762d1790efa44d5b7da98e4d2160b05a6aadec77
andreapesci <andrea.pesci.x3@gmail.com>
2024-06-05 16:59:17 +0200

    template pushed

---
+++ Database/portfolio.js
@@ -0,0 +1,29 @@
+portfolio = [
+    {
+        "id":1,
+        "image":["https://via.placeholder.com/1000x800"],
+        "title":"Captain Bushfire",
+        "description":"Sed non turpis in magna dictum ornare id sit amet orci. Phasellus blandit tortor ante, et condimentum erat facilisis ac. Integer pellentesque lacinia lacinia. Nullam porttitor eget orci ac finibus. ",
+        "roles":["Gameplay Programmer", "Game Designer"],
+        "year":"2024",
+        "tools":["Unreal Engine 5"]
+    },
+    {
+        "id":2,
+        "image":["https://via.placeholder.com/1000x800"],
+        "title":"Captain Bushfire",
+        "description":"Sed non turpis in magna dictum ornare id sit amet orci. Phasellus blandit tortor ante, et condimentum erat facilisis ac. Integer pellentesque lacinia lacinia. Nullam porttitor eget orci ac finibus. ",
+        "roles":["Gameplay Programmer", "Game Designer"],
+        "year":"2024",
+        "tools":["Unreal Engine 5"]
+    },
+    {
+        "id":3,
+        "image":["https://via.placeholder.com/1000x800"],
+        "title":"Captain Bushfire",
+        "description":"Sed non turpis in magna dictum ornare id sit amet orci. Phasellus blandit tortor ante, et condimentum erat facilisis ac. Integer pellentesque lacinia lacinia. Nullam porttitor eget orci ac finibus. ",
+        "roles":["Gameplay Programmer", "Game Designer"],
+        "year":"2024",
+        "tools":["Unreal Engine 5"]
+    }
+]
--- Javascript/script.js
+++ Javascript/script.js
@@ -7,3 +7,48 @@ function updateContent() {
     document.getElementById('some-div').innerHTML = 'Contenuto aggiornato!';
 }
 
+function generateProjectHTML() {
+    console.log('pippo');
+    const div = document.getElementById('swap-content');
+    console.log(div);
+    div.innerHTML = portfolio.map(project => `
+        <div class="project">
+            <div class="project-image-div">
+                <img class="project-image" src="${project.image[0]}" alt="Project Picture"/>
+            </div>
+            <div class="project-text">
+                <div class="project-text-description">
+                    <h2 class="project-text-description-title">${project.title}</h2>
+                    <p class="project-text-description-plot">
+                        ${project.description}
+                    </p>
+                </div>
+                <div class="project-text-information">
+                    <div class="project-text-information-paragraph">
+                        <p class="project-text-information-paragraph-subject">
+                            Tools/Technology
+                        </p>
+                        <p class="project-text-information-paragraph-object">
+                            ${project.tools.join(', ')}
+                        </p>
+                    </div>
+                    <div class="project-text-information-paragraph">
+                        <p class="project-text-information-paragraph-subject">Year</p>
+                        <p class="project-text-information-paragraph-object">
+                            ${project.year}
+                        </p>
+                    </div>
+                    <div class="project-text-information-paragraph">
+                        <p class="project-text-information-paragraph-subject">
+                            Roles
+                        </p>
+                        <p class="project-text-information-paragraph-object">
+                            ${project.roles.join(', ')}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `).join('');
+}
+
